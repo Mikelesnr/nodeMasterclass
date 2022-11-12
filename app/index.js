6be012ff89410080c394ad0ba6e8db62f7f21c86
Mikelesnr <mwanza.n.m@gmail.com>
2022-11-12 14:46:23 +0200

    setting up the server

---
+++ app/index.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+/*
+*Primary filr for the API
+*
+*/
+
+//Dependencies
+const http = require('http');
+
+//The server sshould respond to all requests with a string
+const server = http.createServer(function (req, res) {
+    res.end('Hello World!\n');
+})
+
+
+//Start the server, and have it listen on port 3000
+server.listen(3000, function () {
+    console.log("the server is listening on port 3000");
+})
