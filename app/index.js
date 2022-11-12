7d95e6e6e2a9fb70f422b4d4764581ec76648c57
Mikelesnr <mwanza.n.m@gmail.com>
2022-11-12 15:41:59 +0200

    parsing headers

--- app/index.js
+++ app/index.js
@@ -7,10 +7,34 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
 
 //The server sshould respond to all requests with a string
 const server = http.createServer(function (req, res) {
+    //Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    //Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+\/+$/g, '');
+
+    //Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    //Get the http method
+    const method = req.method.toLocaleLowerCase();
+
+    //Get the headers as an object
+    const headers = req.headers;
+
+    //send the response
     res.end('Hello World!\n');
+
+
+    //Log the request path
+    console.log('Request is recieved with these headers: ', headers);
+
+
 })
 
 
